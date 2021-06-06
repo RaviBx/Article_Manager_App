514ccda8ffd9498caf9e8e04f21d2a6e70fd91ba
RaviBx <ravimuay@gmail.com>
2021-06-06 16:01:09 +0530

    Added article table

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Factory;
+use Faker\Factory as FakerFactory;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Database\Seeder;
+
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        //factory(App\Articles::class, 30)->create();
+       
+        
+    }
+}
